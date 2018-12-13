5950f81e6200e932faf1018291130838260ed4ee
Brxn16 <camplese123@gmail.com>
2018-12-13 13:33:13 -0600

    update

---
+++ DebugSix02/DebugSix02/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+namespace DebugSix02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 12, 15, 22, 88 };
+            int x;
+            double average;
+            double total = 0;
+            Write("\nThe numbers are...");
+            for (x = 0; x < numbers.Length; ++x)
+                Write("{0, 6}", numbers[x]);
+            WriteLine();
+            for (x = 0; x < numbers.Length; ++x)
+            {
+                total = numbers[x];
+            }
+            average = total / numbers.Length;
+            WriteLine("The average is {0}", average);
+        }
+    }
+}
