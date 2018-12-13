f5e2e6c3125309cfec0477b3588e46089f106754
Brxn16 <camplese123@gmail.com>
2018-12-13 12:04:36 -0600

    update

---
+++ CheckZips/CheckZips/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+namespace CheckZips
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] zips = { 67777, 45555, 87654, 12345, 43527, 19476, 47385, 48676, 49337, 85633 };
+            int userZip = 0;
+            int result = 0;
+
+            WriteLine("Enter a 5 digit zip code to see if you area code is eligible for us to deliver to you. >>> ");
+            userZip = Convert.ToInt32(ReadLine());
+
+            for (int i = 0; i < 1; i++)
+            {
+                if (zips[i] == userZip)
+                {
+                    WriteLine("We can deliver to your area!");
+                    result = 1;
+                }
+                else
+                {
+                    WriteLine("We do not deliver to your area");
+                    result = 1;
+                }
+            }
+        }
+    }
+}
--- ScoresComparison/ScoresComparison/Program.cs
+++ ScoresComparison/ScoresComparison/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace ScoresComparison
 {
@@ -10,9 +11,24 @@ namespace ScoresComparison
     {
         static void Main(string[] args)
         {
-            int[,] zips =
+            int[] tests = new int[4];
+            int previousScore = 0;
+            for (int i = 0; i < 4; i++)
             {
-                { }
+                Write("Enter 4 test scores >> ");
+                tests[i] = Convert.ToInt32(ReadLine());
+            }
+            for (int j = 0; j < tests.Length; j++)
+            {
+                if (tests[j] > previousScore)
+                {
+                    previousScore = tests[j];
+                    WriteLine("Congrats, this is the newest high score!");
+                }
+                else if (tests[j] < previousScore)
+                {
+                    WriteLine("Sadly you did not score higher than the highest score! You Suck!!!");
+                }
             }
         }
     }
